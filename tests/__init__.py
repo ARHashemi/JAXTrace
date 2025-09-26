8128b98524647b648be1c8934f06295017808fbe
ARHashemi <arezahashemi@gmail.com>
2025-09-26 13:18:41 +0200

    tests

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+"""
+JAXTrace Test Suite
+
+Basic test utilities for package validation:
+- smoke_test.py: Import and functionality smoke tests
+- structure_test.py: Package structure validation
+"""
